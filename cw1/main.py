9d90434171b152287a569c37de83f05f4ed83cd3
Kornelia <grygo96@o2.pl>
2021-10-13 22:47:46 +0200

    zadania 1 - 13

--- cw1/main.py
+++ cw1/main.py
@@ -32,7 +32,7 @@ print(
 
 alicja = 'ala ma kota'
 print(dir(alicja))
-help(alicja.count())
+help(alicja.upper())
 
 #def count(self, sub, start=None, end=None):  # real signature unknown; restored from __doc__
 #   S.count(sub[, start[, end]]) -> int
@@ -44,3 +44,92 @@ help(alicja.count())
 #    return 0
 
 #zadanie_5
+
+imie = "Kornelia"
+nazwisko = "Grygo"
+
+odw_imie = imie[::-1].lower()
+odw_nazwisko = nazwisko[::-1].lower()
+
+print(f"{odw_imie[0].upper() + odw_imie[1:]} {odw_nazwisko[0].upper() + odw_nazwisko[1:]}")
+
+#zadanie_6
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+lista_2 = lista[5:]
+
+for x in range(0,5):
+    lista.pop()
+
+print(lista)
+print(lista_2)
+
+#zadanie_7
+
+lista.extend(lista_2)
+lista.insert(0,0)
+lista_kopia = lista.copy()
+lista_kopia.reverse()
+
+print(lista_kopia)
+
+#zadanie_8
+
+student_1 = (145012, "Andrzej Piaseczny")
+student_2 = (145514, "Michał Wiśniewski")
+student_3 = (146016, "Dorota Rabczewska")
+lista_studentow = [student_1, student_2, student_3]
+print(lista_studentow)
+
+#zadanie_9
+
+student_11 = (145012, ["Andrzej Piaseczny", "lat 21", "andrzej@gmail.com", 2000, "ul. Wesoła 99 40-222 Biskupiec"])
+student_12 = (145514, ["Michał Wiśniewski", "lat 23", "michal@gmail.com", 1998, "ul. Słoneczna 77 10-089 Olsztyn"])
+student_13 = (146016, ["Dorota Rabczewska", "lat 25", "dorota@gmail.com", 1996, "ul. Kolorowa 66 16-400 Suwałki"])
+lista_studentow = [student_11, student_12, student_13]
+
+slownik = dict(lista_studentow)
+print(slownik)
+
+#zadanie_10
+
+numery = [500200200, 510777444, 500200200, 505234234, 501222333, 510777444]
+numery_2 = set(numery)
+print(numery_2)
+
+#zadanie_11
+
+for element in range(1,11):
+    print(element)
+
+#zadanie_12
+
+for element_2 in range(100,19,-5):
+    print(element_2)
+
+#zadanie_13
+
+slownik_1 = {
+    1: "ala",
+    2: "ma",
+    3: "kota"
+}
+slownik_2 = {
+    1: "asia",
+    2: "lubi",
+    3: "frytki"
+}
+slownik_3 = {
+    1: "andrzej",
+    2: "pływa",
+    3: "wolno"
+}
+
+lista_slownikow = [slownik_1, slownik_2, slownik_3]
+
+pusty_string = ""
+for slowniczek in lista_slownikow:
+    for wartosc in slowniczek.values():
+        pusty_string = pusty_string + wartosc + ", "
+
+print(pusty_string.rstrip(", "))
